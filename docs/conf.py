14deab0a49e8695138fb95521771b7ff8b9fbfb2
Eric Thrift <ericdthrift@gmail.com>
2020-11-01 18:28:02 -0600

    Update docs/conf.py for RTD

--- docs/conf.py
+++ docs/conf.py
@@ -10,19 +10,19 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'odea'
-copyright = '2019, Eric Thrift'
+copyright = '2020, Eric Thrift'
 author = 'Eric Thrift'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '1.0a'
 
 
 # -- General configuration ---------------------------------------------------
